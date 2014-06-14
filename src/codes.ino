622d3a17fef6f25041215936fb027f40d8a2cdc7
Curtis Badke <curtis@badkid.ca>
2014-06-14 07:57:53 -0600

    record codes from Morse Telegraph Club Inc. doc

---
+++ src/codes.ino
@@ -0,0 +1,76 @@
+"A" ".-"
+"B" "-..."
+"C" ".. ."
+"D" "-.."
+"E" "."
+"F" ".-."
+"G" "--."
+"H" "...."
+"I" ".."
+"J" "-.-."
+"K" "-.-"
+"L" "_" //???
+"M" "- -" //???
+"N" "-."
+"O" ". ."
+"P" "....."
+"Q" "..-."
+"R" ". .."
+"S" "..."
+"T" "-"
+"U" "..-"
+"V" "...-"
+"W" ".--"
+"X" ".-.."
+"Y" ".. .."
+"Z" "... ."
+"1" ".--."
+"2" "..-.."
+"3" "...-."
+"4" "....-"
+"5" "---" //???
+"6" "......"
+"7" "--.."
+"8" "-...."
+"9" "-..-"
+"0" "___" //???
+"." "..--.." //period
+":-" "KX" //colon dash
+":\"" "KQ" //colon quotation?
+":" "KO" //colon
+";" "SI" //semicolon
+"," ".-.-" //comma
+"?" "-..-." //interrogation
+"!" "---." //exclamation
+"/fraction1/" "E" //fraction line
+"/fraction2/" "UT" //fraction line
+"_" "DX" //dash
+"=" "BK" //double dash
+"-" "HX" //hyphen
+"'" "QX" //apostrophe
+"$" "SX" //dollars
+"%" "OSO" //percent
+"/cents/" "C" //cents
+"/pounds/" "LX" //pounds
+"/shillings/" "UT" //shillings
+"/pence/" "D" //pence
+"/capital/" "CX" //capital letter
+"/small/" "I5" //small letter
+"/dot/" "DOT" //decimal point
+"/paragraph/" "----" //paragraph //???
+"/notcode/" "E5" //not code
+"/underline/" "UX" //underline
+"/endline/" "UJ" //underline (end)
+"(" "PN" //parenthesis (start)
+")" "PY" //parenthesis (end)
+"/brackets/" "BX" //brackets
+"/quote/" "QN" //quotation (start)
+"/endquote/" "QJ" //quotation (end)
+
+"/repeat/" "DN" //repeat when sending
+"/understood/" "13" //I understand
+"/broadcast/" "23" //message to all offices
+"/busy/" "25" //I am busy
+"/break/" "BK" //break or stop sending
+"/finish/" "30" //finish (no more)
+"/regards/" "73" //regards
